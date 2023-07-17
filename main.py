6fa62a0d3d432e52ecc95f4ac8c8f6d4493a5157
Goriaev <138527976+Goriaev@users.noreply.github.com>
2023-07-16 21:07:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def my_f(s_1, s_2):
-    def my_f2(s_3,s_4):
-        return s_3,s_4
-    return s_1
-print(my_f(10,20))
-print(my_f2("dima", "sariev"))
+def int_func(slovo):
+    return slovo.title()
+
+print(int_func('TEXT'))
+
+
+def int_func(slovo):
+    return slovo.title()
+
+print(int_func('text'))
